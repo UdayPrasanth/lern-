fe759aecab4013375a033ad0f2a0ed66b3e8ff9b
Veerandra Mohan <veeru.sripadam@gmail.com>
2025-11-05 22:57:02 -0500

    Initial build

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,4 +1,4 @@
 // frontend/src/apiConfig.js
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://lern-mern-project.onrender.com';
 
 export default API_URL;
